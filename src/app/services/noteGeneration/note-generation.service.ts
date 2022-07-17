57872aa288aa66f284ffcabbce1e3e60cd5666c5
Ian Eduardo Velazquez <ivelazquezenriquez@griddynamics.com>
2022-07-17 14:11:05 -0500

    commit last changes

--- src/app/services/noteGeneration/note-generation.service.ts
+++ src/app/services/noteGeneration/note-generation.service.ts
@@ -26,6 +26,12 @@ export class NoteGenerationService {
           color: '#047886'
         },
         {
+          text: '777-228-03-76',
+          fontSize: 13,
+          alignment: 'center',
+          color: '#047886'
+        },
+        {
           text: 'RECIBO',
           fontSize: 20,
           bold: true,
@@ -49,6 +55,14 @@ export class NoteGenerationService {
               { text: this.invoice.contactNo } */
             ],
             [
+              {
+                text: `WhatsApp: 777-120-41-67`,
+                alignment: 'right'
+              },
+              {
+                text: `Email: dicoprint14@hotmail.com`,
+                alignment: 'right'
+              },
               {
                 text: `Fecha: ${new Date(data?.noteDate).toLocaleString()}`,
                 alignment: 'right'
@@ -95,8 +109,25 @@ export class NoteGenerationService {
         {
           text: 'Instrucciones adicionales',
           style: 'sectionHeaderMargin'
-        }
+        },
+        { 
+          text: `TODO TRABAJO REQUIERE DE UN 50% DE ANTICIPO PARA SU ELABORACIÓN / TODA MODIFICACIÓN DE ARCHIVOS GENERA UN COSTO ADICIONAL / REVISE SU MATERIAL, SALIDA LA MERCANCIA
+          NO SE ACEPTAN RECLAMACIONES NI DEVOLUCIONES / RECOGA SU MATERIAL A TIEMPO, TRANSCURRIDOS 20 DIAS DESPUES DE LA FECHA DE ENTREGA, NO NOS HACEMOS RESPONSABLES
+          POR ÉSTE / EN TODA CANCELACIÓN SE COBRARÁ UN 20% DEL TOTAL DEL PEDIDO/ CUALQUIER TRABAJO URGENTE GENERA UN COSTO ADICIONAL DEL 30% SOBRE NUESTROS PRECIOS
+          CONVENCIONALES / EN TODO TRABAJO DE DISEÑO NUESTRA RESPONSABILIDAD TERMINA CUANDO USTED AUTORIZA LA IMPRESION DEL MISMO. LE SUPLICAMOS REVISAR A DETALLE SUS
+          ORIGINALES DIGITALES ANTES DE IMPRIMIR EN NUESTRAS INSTALACIONES O EN LA IMPRENTA DE SU PREFERENCIA POR LO QUE NO NOS RESPONSABILIZAMOS POR NEGATIVOS, PLACAS,
+          PAPEL O NINGÚN OTRO MATERIAL ESTROPEADO A CONSECUENCIA DE ERRORES DE DISEÑO (FALTAS DE ORTOGRAFÍA, REDACCIÓN, FOTOS ERRONEAS, ETC).
+          ESTE DOCUMENTO ES UNICAMENTE PARA CONTROL INTERNO, NO TIENE NINGUNA VALIDEZ FISCAL
+          `,
+          style: 'disclaimer',
+          alignment: 'center'
+        },
       ],
+      footer: {
+        columns: [
+         
+        ]
+      },
       styles: {
         sectionHeader: {
           bold: true,
@@ -109,8 +140,20 @@ export class NoteGenerationService {
           decoration: 'underline',
           fontSize: 14,
           margin: [0, 15,0, 15]   
+        },
+        disclaimer: {
+          bold: false,
+          fontSize: 6,
+          margin: [0, 60,0, 15],
+          color: '#9a9a9a' 
+        },
+        disclaimer2: {
+          bold: false,
+          fontSize: 6,
+          margin: [0, 0,0, 15],
+          color: '#838383' 
         }
-      }
+      },
     };
 
     pdfMake.createPdf(docDefinition).print();
@@ -157,6 +200,10 @@ export class NoteGenerationService {
             [
               {
                 text: `Fecha: ${new Date(data?.noteDate).toLocaleString()}`,
+                alignment: 'right'
+              },
+              { 
+                text: `Id venta : ${data?._id}`,
                 alignment: 'right'
               }
             ]
